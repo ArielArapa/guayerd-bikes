a05f12a9db9c29ca2b162e8cc76414311fb0025e
Ariel <ariel_angel00@outlook.com>
2021-12-15 15:44:12 -0300

    mostrar productos de objeto en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Locura from './pages/productos/ProductoUnitario';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Productos from './pages/productos/Productos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => (
+  <>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Productos />} />
+        <Route path="producto" element={<Locura />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+)
 
 export default App;
