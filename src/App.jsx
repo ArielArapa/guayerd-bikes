4e4e3abc24e232df7e46906339e95446ecceceb0
daniela <daniela24051989@gmail.com>
2021-12-16 18:58:53 -0300

    historia

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Historia from './pages/historia/Componente';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+<>
+<Historia/>
+</>
+
+);
+
+
 
 export default App;
