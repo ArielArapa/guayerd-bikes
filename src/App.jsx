85ed2a133b3d79f7e78bb18061af99d8eba15c42
daniela <daniela24051989@gmail.com>
2021-12-19 17:31:50 -0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import Historia from './pages/historia/Componente';
+import Descont from "./pages/Descuento/index.jsx";
+
 
 const App = () => (
 <>
 <Historia/>
+<Descont/>
 </>
 
 );
---
+++ src/pages/Descuento/index.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+/* import { Link } from "react-router-dom" */
+const Descont = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        // Fetch data cuando se monta el componente
+        fetch(" https://demo2420474.mockable.io/getCoupon ") //lamada de API
+        .then((res) => res.json()) //status 200 //  error = 404
+        .then((data) =>
+
+        // Guarda posts en estado
+        setPosts(data)
+        )
+    }, 
+    [setPosts] );// <-- No se paso el segundo argumento ([]). que sucederá?
+
+           return(
+        <>
+     <h1>descuento: {posts.text}   </h1> 
+     <h1>discountPercentage: {posts.text}   </h1> 
+          </>)
+     }
+   
+         
+     export default Descont;
+   
