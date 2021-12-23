0c56bae1ee32f0012090c096de96d7db4e0d84b0
Ariel <ariel_angel00@outlook.com>
2021-12-23 00:17:47 -0300

    recuperar datos de api productos

--- src/pages/productos/index.jsx
+++ src/pages/productos/index.jsx
@@ -1,11 +1,13 @@
 import Producto from "./producto"
 import style from "./style";
+import Prueba from "./prueba";
 
 const Productos = () => (
     <>
         <h2 style={style.tituloIndex} id="Productos">Productos</h2>
         <div style={style.cardIndex}>
-            <Producto />
+            {/* <Producto /> */}
+            <Prueba tipo={"carrera"} />
         </div>
     </>
 )
---
+++ src/pages/productos/prueba.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import style from "./style";
+
+const Prueba = ({ tipo }) => {
+
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        // Fetch data cuando se monta el componente
+        fetch(`https://61c3d07af1af4a0017d990ab.mockapi.io/${tipo}`) //lamada de API y 
+            .then((res) => res.json()) //status 200 //  error = 404
+            .then((data) => setPosts(data)) // Guarda posts en estado
+    }, [tipo]);// <-- No se paso el segundo argumento ([]). que sucederá?
+
+    return (
+        <>
+            {posts.map((element) => {
+                return (
+                    <>
+                        <div style={style.cardProducto} key={element.id}>
+                            <img src={element.img} alt={element.nombre} style={style.imgProducto} />
+                            <div style={style.cardContentProducto}>
+                                <span>{element.stock}</span>
+                                <p>Precio actual: {element.precio}</p>
+                                <h1>{element.nombre}</h1>
+                                <p>{element.descripcion}</p>
+                                <button>Ver más</button>
+                                {/* <Link to="producto">Ver más</Link> */}
+                            </div>
+                        </div>
+                    </>)
+            })}
+        </>
+    )
+}
+
+export default Prueba;
